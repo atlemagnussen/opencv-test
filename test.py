054cb59374f00ba5c349d726d1da0ac2b03a3398
Atle Magnussen <atlemagnussen@gmail.com>
2016-08-19 15:40:26 +0200

    works to stream from http mpeg and local avi file, added some links that helped in solving the encoding shit

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 import numpy as np
 import cv2
 
-#cap = cv2.VideoCapture("http://admin:einherjer57@57.57.57.102/video2.mjpg")
+cam1 = "http://admin:einherjer57@57.57.57.102/video1.mjpg"
+cam2 = "http://admin:einherjer57@57.57.57.102/video2.mjpg"
+cam3 = "http://admin:einherjer57@57.57.57.102/video3.mjpg"
 
-cap = cv2.VideoCapture("video.avi")
+cap = cv2.VideoCapture(cam3)
 
+#cap = cv2.VideoCapture("video.avi")
+i = 0
 while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    print("isOpened=" + str(cap.isOpened()))
+    i += 1
+    print("isOpened=" + str(cap.isOpened()) + "pic " + str(i) + "ret " + str(ret))
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
