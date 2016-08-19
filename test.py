a0218b0ad829abf2bdf0f98f8946424c780e649f
Atle Magnussen <atlemagnussen@gmail.com>
2016-08-19 14:52:32 +0200

    test different reads, ignore avi file

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture("http://admin:einherjer57@57.57.57.102/video2.mjpg")
 
-while(True):
+cap = cv2.VideoCapture("video.avi")
+
+while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
+    print("isOpened=" + str(cap.isOpened()))
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -15,6 +17,7 @@ while(True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+print("isOpened=" + str(cap.isOpened()))
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
